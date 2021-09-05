1a17e4733b46b233dc1ca8c6041149edb2481b9a
nabin <nabin@dzangolab.com>
2021-09-05 10:26:07 +0545

    feat: add simple validation rule

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
+var cors = require('cors')
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.post('/post-pin', (req, res) => {
-    console.log('Got body:', req.body);
-    res.sendStatus(200);
+app.post('/pin', (req, res) => {
+    const pin = req.body.pin;
+    if(!pin || pin.length !==6 || pin[5]===7){
+        res.status(200).json({message:"Invalid"});
+    }
+
+    res.status(200).json({message:"valid"});
 });
 
 app.listen(8080, () => console.log(`Started server at http://localhost:8080!`));
